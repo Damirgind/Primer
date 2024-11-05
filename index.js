53e984024737337255d1c7fc6bd86e4957c321c6
Damirgind <gindullindamir777@gmail.com>
2024-11-05 17:49:28 +0300

    Axios 2.0

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ bot.command('new_buy', async ctx => await NEWBUYCommand(ctx))
 // ---------------------------------------------------------------------
 // ПРОГРАММА БОТА
 
-const KEEP_ALIVE_INTERVAL = 5 * 60 * 10 // 5 минут
+const KEEP_ALIVE_INTERVAL = 4 * 60 * 1000 // 4 минуты
 
 function keepBotAlive() {
 	setInterval(async () => {
